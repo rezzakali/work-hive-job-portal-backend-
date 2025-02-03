11defbe122255492be44312a1d23ae7c6558df52
rezzak ali <rezzak ali>
2025-02-03 09:54:46 +0530

    add preflight requests for all routes

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Allow preflight requests for all routes
+app.options('*', cors());
 // cors-policy
 app.use(cors(corsOptions));
 
