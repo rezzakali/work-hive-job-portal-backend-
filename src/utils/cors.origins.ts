e62035cbb99e6597cc912df4f0a922fa0b7cb34b
rezzak ali <rezzak ali>
2025-02-04 16:07:43 +0530

    update on cors.config file

--- src/utils/cors.origins.ts
+++ src/utils/cors.origins.ts
@@ -6,6 +6,7 @@ const origins =
     ? ['http://localhost:3000', 'http://localhost:5173'] // Development front-end origin and dashboard
     : [
         'https://job-portal-dashboard.vercel.app',
+        'https://workshive.vercel.app',
         'https://workshive.netlify.app',
       ]; // Production front-end origin dashboard
 
