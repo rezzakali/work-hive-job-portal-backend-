a0d3c6c66400b786adc4c73068d4aaab19cf7ee9
rezzak ali <rezzak ali>
2025-02-05 15:24:14 +0530

    implement FCM

--- dist/utils/getFutureDate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getFutureDate = (days) => {
-    const date = new Date();
-    date.setDate(date.getDate() + days);
-    return date;
-};
-exports.default = getFutureDate;
